2bd239bc9331326dd9934b50d443c52a59dc88fd
LanceEnr <lancemikaelo.enriquez@gmail.com>
2024-02-29 21:04:52 +0800

    modal component

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,7 +1,6 @@
 <template>
   <div
-    class="w-full rounded-lg shadow-md lg:max-w-sm"
-    style="cursor: 'pointer'"
+    class="w-full rounded-lg shadow-md lg:max-w-sm cursor-pointer"
     @click="showModal = true"
   >
     <img
@@ -15,34 +14,61 @@
     <div class="p-4 bg-white rounded-b-lg">
       <h4 class="text-l font-semibold">{{ post.title }}</h4>
       <p class="mb-2 leading-normal text-xs">{{ post.content }}</p>
-      <p class="mb-2 leading-normal font-semibold text-xs">{{ post.user }}</p>
+      <p class="mb-2 leading-normal font-semibold text-xs text-yellow-500">
+        {{ post.user }}
+      </p>
     </div>
 
-    <div v-if="showModal" id="large-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">      <div class="relative w-full max-w-4xl max-h-full">
+    <div
+      v-if="showModal"
+      id="large-modal"
+      tabindex="-1"
+      class="fixed inset-0 z-50 flex items-center justify-center h-full overflow-x-hidden overflow-y-auto"
+      style="background-color: rgba(0, 0, 0, 0.5)"
+    >
+      <div class="relative max-h-full">
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <div class="relative bg-white rounded-lg shadow">
           <!-- Modal header -->
-          <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-            <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+          <div
+            class="flex items-center justify-between p-4 md:p-5 border-b rounded-t"
+          >
+            <h3 class="text-xl font-medium text-gray-900">
               {{ post.title }}
             </h3>
-            <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" @click="showModal = false">
-              <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            <button
+              type="button"
+              class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+              @click="showModal = false"
+            >
+              <svg
+                class="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                />
               </svg>
               <span class="sr-only">Close modal</span>
             </button>
           </div>
           <!-- Modal body -->
           <div class="p-4 md:p-5 space-y-4">
-      <img :src="post.img" alt="image" class="modal-image">
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            <img :src="post.img" alt="image" class="modal-image" />
+            <p class="text-base leading-relaxed text-gray-500">
               {{ post.content }}
             </p>
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            <p class="text-base leading-relaxed text-gray-500">
               {{ post.date }}
             </p>
-            <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            <p class="text-base leading-relaxed text-gray-500">
               {{ post.user }}
             </p>
           </div>
@@ -54,10 +80,10 @@
 
 <script>
 export default {
-  props: ['post'],
+  props: ["post"],
   data() {
     return {
-      showModal: false,  // Make sure this is initially set to false
+      showModal: false,
     };
   },
 };
